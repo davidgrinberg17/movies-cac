6ef4b1517b0256b47e6aa2e43211cdb196de3536
davidgrinberg17 <davidgrinberg17@hotmail.com>
2023-06-06 12:55:58 -0300

    clienteJS

---
+++ src/utils/httpCliente.js
@@ -0,0 +1,11 @@
+const API = "https://api.themoviedb.org/3"
+
+export const get = (path) => {
+    return fetch (API+path, {
+        headers:{
+            Authorization:
+            "Bearer",
+            "Content-Type": "application/json;charset=utf-8"
+        }
+    }).then((results) => results.json())
+}
